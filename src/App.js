6b49a0c5db402f5d4f3827ce1e3be3bb814571ce
damien lewis <damien_lewis2k12@hotmail.com>
2023-04-04 13:56:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [allCharacters, setAllCharacters] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+      const response = await fetch("https://anapioficeandfire.com/api/characters/1")
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      const data = await response.json();
+      console.log(data);
+      setAllCharacters(data);
+      } catch (err) {
+        console.log(err);
+        setErrorMsg("Data Not Found");
+      }
+    }
+    fetchData();
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetching data</h1>
+      {errorMsg && <h3>{errorMsg}</h3>}
+      {allCharacters.map((character, index) => {
+        return <p key={index}>{character.name}, {character.species}</p>
+      })}
     </div>
   );
 }
